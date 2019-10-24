9126f2621de7247c6a6cb00f972f4aef852351e6
Shelby Drabant <drabants@gmail.com>
2019-10-23 23:27:50 -0700

    Added Movement of Plate
    Repositioned all object to normalize starting positons to 0,0,0
    
    Added controllers for Menu, and Plate.

--- Get It In The Bin/Assets/Scripts/PlateController.cs
+++ Get It In The Bin/Assets/Scripts/PlateController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlateController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject tableObj;
+
+    private float movementSpeed = 4f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(transform.position.x, transform.position.y, 10f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.z > tableObj.transform.position.z)
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * movementSpeed);
+        }
     }
 }
